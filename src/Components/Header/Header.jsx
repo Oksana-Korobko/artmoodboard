e7e5f9248a7de908fdbbe0e28886b17f70b470ea
Oksana <oksana.korobko.fd@gmail.com>
2024-06-27 23:13:09 +0300

    Header layout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,9 +5,12 @@ export default function Header() {
   return (
     <div>
       <div className='title'>inspiration</div>
-      <div className='login'>Login</div>
-      <div className='language'>UA</div>
-      <div className='theme'>Theme</div>
+      <div className='titlePanel'>
+        <div className='login'>Login</div>
+        <div className='language'>UA</div>
+        <div className='theme'>Theme</div>
+      </div>
+      
     </div>
   )
 }
