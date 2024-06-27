fff79370620fc907414d8555f13f93b9c7e70426
Oksana <oksana.korobko.fd@gmail.com>
2024-06-27 23:08:18 +0300

    Header1

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,6 +3,11 @@ import './Header.css'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div className='title'>inspiration</div>
+      <div className='login'>Login</div>
+      <div className='language'>UA</div>
+      <div className='theme'>Theme</div>
+    </div>
   )
 }
