6df6ec5cef2900232d6352d681b804fb7cb19245
Oksana <oksana.korobko.fd@gmail.com>
2024-06-28 21:55:59 +0300

    get photos from pexels

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import './App.css';
 import Filters from './Components/Filters/Filters';
 import Header from './Components/Header/Header';
 import Board from './Components/Board/Board'
+import { useEffect } from 'react';
+import { createClient } from 'pexels';
+
+// bRl7IXvNx9GykmRGXzIpSjMKVPv6KA369K4oMyf7KKEimiQLJHHSMuXf
 
 function App() {
+  const data = [];
+  const client = createClient('bRl7IXvNx9GykmRGXzIpSjMKVPv6KA369K4oMyf7KKEimiQLJHHSMuXf');
+  const query = 'Nature';
+  
+  useEffect(() => {    
+      client.photos.search({query, per_page: 20 }).then(photos => {
+      if (data.length===0) {data.push(...photos.photos)}      
+    });    
+    console.log(data)
+  }, [])
+
   return (
     <div className="App">
       <Header />
       <Filters />
-      <Board />
+      <Board data = {data}/>
     </div>
   );
 }
