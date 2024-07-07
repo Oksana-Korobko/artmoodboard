71de1943e64cb77da2b586b820e191c0fa19e857
Oksana <oksana.korobko.fd@gmail.com>
2024-07-07 19:25:19 +0300

    grid-07-07

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import './App.css';
+// import '../node_modules/react-grid-layout/css/styles.css'
+// import '../node_modules/react-resizable/css/styles.css'
 import Filters from './Components/Filters/Filters';
 import Header from './Components/Header/Header';
 import Board from './Components/Board/Board'
-import { useEffect, useState } from 'react';
-import { createClient } from 'pexels';
+// import { useEffect, useState } from 'react';
+// import { createClient } from 'pexels';
 
 // bRl7IXvNx9GykmRGXzIpSjMKVPv6KA369K4oMyf7KKEimiQLJHHSMuXf
 
 function App() {
-  const [data,setData] = useState([]);
-  const client = createClient('bRl7IXvNx9GykmRGXzIpSjMKVPv6KA369K4oMyf7KKEimiQLJHHSMuXf');
-  const query = 'Nature';
-  const orientation = 'square';
+  // const [data,setData] = useState([]);
+  // const client = createClient('bRl7IXvNx9GykmRGXzIpSjMKVPv6KA369K4oMyf7KKEimiQLJHHSMuXf');
+  // const query = 'pink';
+  // const orientation = 'square';
   
-  useEffect(() => {    
-      client.photos.search({query, orientation, per_page: 9 }).then(photos => {
-      if (data.length===0 && photos) { setData(photos.photos)} 
+  // useEffect(() => {    
+  //     client.photos.search({query, orientation, per_page: 9 }).then(photos => {
+  //     if (data.length===0 && photos) { setData(photos.photos)} 
            
-    });      
-  }, [])
+  //   });      
+  // }, [])
   
   
   return (
     <div className="App">
       <Header />
       <Filters />
-      <Board data = {data}/>
+      <Board />
     </div>
   );
 }
--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,19 +1,66 @@
 import React, { useState } from 'react'
+import { useEffect} from 'react';
+import { createClient } from 'pexels';
 import './Board.css'
+import '../../../node_modules/react-grid-layout/css/styles.css'
+import '../../../node_modules/react-resizable/css/styles.css'
+import GridLayout from "react-grid-layout";
+
 import MainPicture from '../MainPicture/MainPicture'
 import PictureItem from '../PictureItem/PictureItem'
 
-export default function Board({data}) {
+export default function Board() {
   const [main, setMain] = useState(0)
   // const [page, setPage] = useState(0)
+  const [data,setData] = useState([]);
+  const client = createClient('bRl7IXvNx9GykmRGXzIpSjMKVPv6KA369K4oMyf7KKEimiQLJHHSMuXf');
+  const query = 'forest';
+  const orientation = 'square';
+
+  const  layout  =  [ 
+    {  i : "main" ,  x : 0 ,  y : 0 ,  w : 2 ,  h : 2 } , 
+    {  i : "i1" ,  x : 2 ,  y : 0 ,  w : 1 ,  h : 1 } , 
+    {  i : "i2" ,  x : 3 ,  y : 0 ,  w : 1 ,  h : 1 } ,
+    {  i : "i3" ,  x : 2 ,  y : 1 ,  w : 1 ,  h : 1 } ,
+    {  i : "i4" ,  x : 3 ,  y : 1 ,  w : 1 ,  h : 1 } ,
+    {  i : "i5" ,  x : 0 ,  y : 2 ,  w : 1 ,  h : 1 } ,
+    {  i : "i6" ,  x : 1 ,  y : 2 ,  w : 1 ,  h : 1 } ,
+    {  i : "i7" ,  x : 2 ,  y : 2 ,  w : 1 ,  h : 1 } ,
+    {  i : "i8" ,  x : 3 ,  y : 2 ,  w : 1 ,  h : 1 } 
+
+    
+  ] ; 
+  
+  useEffect(() => {    
+      client.photos.search({query, orientation, per_page: 9 }).then(photos => {
+      if (data.length===0 && photos) { setData(photos.photos)} 
+           
+    });      
+  }, [])
+  
+  
   
   return (
-    <div className='grid-container'>
-      <MainPicture elem = {data[main]}/>
-      {data.map((item) => (        
-        <PictureItem item={item} key = {item.id}/>
-      ))}
+
+    < GridLayout 
+        className = "layout" 
+        layout = { layout } 
+        cols = { 12 } 
+        rowHeight = { 30 } 
+        width = { 1200 } 
+      >
+        <div key="a">a</div>
+        <div key="b">b</div>
+        <div key="c">c</div>
+      </GridLayout>
+
+
+    
       
-    </div>
+      // { {data.map((item) => (        
+      //   <PictureItem item={item} key = {item.id}/>
+      // ))} */}
+      
+    
   )
 }
